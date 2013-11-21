fe3606fb8dc6e0810e0bcfefbf2d25ec813794ef
root <root@ada2.if.ufrgs.br>
2013-11-21 17:50:35 -0200

    Adicionado configs maui.

--- cluster_torque/compile/torque/make_deb.sh
+++ cluster_torque/compile/torque/make_deb.sh
@@ -5,7 +5,7 @@
 #
 SRC="/root/tmp/torque-4.2.6"
 VERSION="4.2.6"
-TMPBUILD="$TMPBUILD"
+TMPBUILD="/tmp/build_torque"
 CONFIGURE_FLAGS="--with-default-server=ada2-mpi --with-server-home=/var/spool/torque --with-rcp=scp --with-pam=yes --enable-nvidia-gpus"
 
 ##
@@ -19,11 +19,12 @@ make
 mkdir -p $TMPBUILD
 make install DESTDIR=$TMPBUILD
 mkdir -p $TMPBUILD/etc/init.d
-cp contrib/init.d/trqauthd $TMPBUILD/etc/init.d/
-cp contrib/init.d/pbs_server $TMPBUILD/etc/init.d/
-cp contrib/init.d/pbs_mom $TMPBUILD/etc/init.d/
+cp contrib/init.d/debian.pbs_sched $TMPBUILD/etc/init.d/pbs_sched
+cp contrib/init.d/debian.trqauthd $TMPBUILD/etc/init.d/trqauthd
+cp contrib/init.d/debian.pbs_server $TMPBUILD/etc/init.d/pbs_server
+cp contrib/init.d/debian.pbs_mom $TMPBUILD/etc/init.d/pbs_mom
 cd $TMPBUILD
-fpm -s dir -t deb -n torque-if -v $VERSION -C $TMPBUILD -p /root/torque-if_"$VERSION"_amd64.deb *
+fpm -s dir -t deb -n torque-if -v $VERSION -C $TMPBUILD -p torque-if_"$VERSION"_amd64.deb *
 cd -
 clear
-echo "Pacote DEB disponivel em $TMPBUILD/ !"
+echo "Pacote DEB disponivel em $TMPBUILD !"
