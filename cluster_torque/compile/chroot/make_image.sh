deb6f9909650c2c6d4f6c0a8936405935b736881
James <james.silva@Mac185941.local>
2013-11-26 11:15:11 -0200

    Adicionando chroot.

--- cluster_torque/compile/chroot/make_image.sh
+++ cluster_torque/compile/chroot/make_image.sh
@@ -1,19 +1,3 @@
-debootstrap --arch amd64 wheezy /home/RESOURCES/CHROOT http://debian.if.ufrgs.br/debian
-LANG=C chroot /home/RESOURCES/CHROOT /bin/bash
-apt-get update
-apt-get install nfs-common libpam-ldap openssh-server
-
-/tmp/torque-4.2.6# make install DESTDIR=/home/RESOURCES/CHROOT
-cp /etc/hosts etc/
-fstab
-
-10.1.0.254:/work/home /work/home nfs defaults 0 0 
-10.1.0.254:/var/spool /spool nfs defaults 0 0
-10.1.0.254:/shared /shared nfs defaults 0 0
-10.1.0.254:/usr/local/cuda /usr/local/cuda nfs defaults 0 0
-
-
-/home/RESOURCES/CHROOT/root/.ssh# cp /root/BKPS-ADA/root/PXE/CHROOT/root/.ssh/authorized_keys .
-
-edita pbs_mom e hostname
-
+multistrap -f files/node.multistrap
+mkinitramfs
+find . | cpio --quiet --dereference -o -H newc | gzip > /tftpboot/nodes/initrdfull.img
